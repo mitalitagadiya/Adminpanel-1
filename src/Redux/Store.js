443117312711c15d83938759cbf6dcc96d87b854
mitalitagadiya <mitalitagadiya@gmail.com>
2022-07-15 06:20:48 -0700

    15

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -4,6 +4,8 @@ import { rootReducer } from './Reducer/Index';
 let store = createStore()
 
 export const configurestore = () => {
+    
     let store = createStore(rootReducer)
+
     return store;
 }
